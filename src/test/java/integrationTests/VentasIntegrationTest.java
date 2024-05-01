f937250bf1b6a4361460b9270d4739b1cefb08c1
Cristian A. Vlad <cav.contact.pr@gmail.com>
2024-05-01 17:56:44 +0200

    Integration tests update

--- src/test/java/integrationTests/VentasIntegrationTest.java
+++ src/test/java/integrationTests/VentasIntegrationTest.java
@@ -1,8 +1,11 @@
 package integrationTests;
 
+import Personajes.Cazador;
+import Personajes.Personaje;
 import Sistema.Sistema;
 import Usuario.Jugador;
 import Ventas.Oferta;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -13,6 +16,10 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class VentasIntegrationTest {
+    private Sistema sharedSistemaInstance;
+    private Jugador sharedSamplePlayer;
+
+    @BeforeEach
     @Test
     // Test: Register a new user and create a new offer, check if that new offer is correctly saved into Sistema
     void registerNewUserAndCreateOffer(){
@@ -20,6 +27,7 @@ public class VentasIntegrationTest {
         Jugador testPlayer = new Jugador(
                 "Jorge", "ItsGeorge", "1234", null, null
         );
+        sharedSamplePlayer = testPlayer;
         // 2) Adding the new offer
         List<Oferta> offerList = new ArrayList<>();
         Oferta testOffer = new Oferta(
@@ -28,28 +36,45 @@ public class VentasIntegrationTest {
         offerList.add(testOffer);
         // 3) Saving offer
         try {
-            Field listaOfertasField = Sistema.class.getDeclaredField("listaOfertas");
+            Field listaOfertasField = Sistema.class.getDeclaredField("listaOfertasNoValidadas");
             listaOfertasField.setAccessible(true);
             Sistema testSystem = new Sistema();
             listaOfertasField.set(testSystem, offerList);
 
-            assertEquals(testOffer, testSystem.getListaOfertas().get(0));
-
+            sharedSistemaInstance = testSystem;
+            assertEquals(testOffer, testSystem.getListaOfertasNoValidadas().get(0));
         } catch (IOException | NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
     }
     @Test
-    void viewAvailableOffersTest(){
-        // Check that if there are not any offer the app displays a message and if there were all offers are show to a user
+    // Test: Check if the created offers are accessible
+    void viewAvailableOffersTest() throws IOException {
+        // 1) If there are not any offers
+        Sistema testSystem = new Sistema();
+        assertEquals(0, testSystem.getListaOfertas().size());
+        // 2) If there is at least one existing offer
+        assertNotEquals(0, sharedSistemaInstance.getListaOfertas().size() + sharedSistemaInstance.getListaOfertasNoValidadas().size());
     }
     @Test
+    // Test: Checks if a purchased items is correctly serialized
     void acceptOfferAndPurchaseTest(){
-        // Checks if a purchased items is correctly serialized
-    }
-    @Test
-    void updateOfferStateAfterPurchaseTest(){
-        // Checks if VentaLog objects are created and serialized
+        // 1) Creating the buyer
+        Personaje testCharacter = new Cazador(
+                "Eren", null, null, null, null, 2000, 3
+        );
+        Jugador testPlayer = new Jugador(
+                "Matt", "Matt4321", "1234", testCharacter, null
+        );
+        // 2) Validating an offer
+        try {
+            Field listaOfertasField = Sistema.class.getDeclaredField("listaOfertas");
+            listaOfertasField.setAccessible(true);
+            listaOfertasField.set(sharedSistemaInstance, sharedSistemaInstance.getListaOfertasNoValidadas());
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        // 3) Verificar el inventario de un jugador
     }
     @Test
     void validateOfferAndPurchaseWithOperatorTest(){
